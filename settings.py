ca05abdfa322c48f89e931d3589edad9a672da82
Kyaw Ko Ko Tun <89926955+KyawKoKoTun@users.noreply.github.com>
2023-03-12 19:46:40 +0630

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,14 +3,7 @@ from datetime import timedelta
 
 
 class Settings(Config):
-    db_user = 'kagyinet_admin'
-    db_password = '6PA7L7)1Q8}('
-    host = '67.211.222.34'
-    db_name = 'kagyinet_database'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db?check_same_thread=False'
-    # SQLALCHEMY_DATABASE_URI = f'mysql+mysqldb://{db_user}:{db_password}@{host}/{db_name}'
-    #SQLALCHEMY_DATABASE_URI = 'postgresql://xkedplapsyckuy:ec466b14127fb63ec29324edfc4a1a2381acffe8c813bd84ad2f20b3a1078387@ec2-54-172-175-251.compute-1.amazonaws.com:5432/d8godpaeugasej'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db?check_same_thread=False'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://admin:aAdVsLOxWiICT3bu6H5yaLIpoK6miI3J@dpg-cg6sv5pmbg5ab7lu5470-a.oregon-postgres.render.com/socket_chat'
     SECRET_KEY = '740d9e58782809e59424751c9b5279af09e707febe831fec78ff1e2571f52b89'
-    # SESSION_PERMANENT = True
-    # PERMANENT_SESSION_LIFETIME = timedelta(days=31)
     SESSION_TYPE = 'filesystem'
