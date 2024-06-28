445285b4673964d9c6ad5dd80a2e1dd10dcda037
Kyaw Ko Ko Tun <kyawkokotunmm475157@gmail.com>
2024-06-29 00:31:20 +0630

    Maked some changes

--- settings.py
+++ settings.py
@@ -1,9 +1,11 @@
 from flask import Config
 from datetime import timedelta
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 class Settings(Config):
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db?check_same_thread=False'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://admin:aAdVsLOxWiICT3bu6H5yaLIpoK6miI3J@dpg-cg6sv5pmbg5ab7lu5470-a.oregon-postgres.render.com/socket_chat'
-    SECRET_KEY = '740d9e58782809e59424751c9b5279af09e707febe831fec78ff1e2571f52b89'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DB')
+    SECRET_KEY = os.environ.get('SECRET')
     SESSION_TYPE = 'filesystem'
